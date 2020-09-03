21f8e906f53997ba08e9e9e1a219f93deee936ec
Hope <hopegiometti@gmail.com>
2020-09-03 11:22:21 -0600

    api up and running

---
+++ app.py
@@ -0,0 +1,38 @@
+
+import flask
+from flask import Flask
+from flask import jsonify 
+import json
+from flask_cors import CORS
+
+
+spells = json.load(open("files/spells.json"))
+
+
+app=Flask(__name__)
+CORS(app)
+
+
+
+@app.route('/')
+
+
+
+def home():
+    allspellnames = []
+
+    for spell in spells:
+        allspellnames.append(spell)
+    
+    return jsonify(allspellnames)
+
+# def home():
+#     return a
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+#to run a flask app
+#flask run 
